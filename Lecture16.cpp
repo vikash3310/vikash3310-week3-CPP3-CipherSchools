de5ceaf9b4844bdb2922b7e32db36210bedcef0c
vikash3310 <119447754+vikash3310@users.noreply.github.com>
2023-07-15 23:34:19 +0530

    Update Lecture16.cpp

--- Lecture16.cpp
+++ Lecture16.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+int* p = new int(25);
+float* q = new float(75.25);
 
+// Custom data type
+struct cust
+{
+	int p;
+	cust(int q) : p(q) {}
+	cust() = default;
+	//cust& operator=(const cust& that) = default;
+};
+
+int main()
+{
+	// Works fine, doesn’t require constructor
+	cust* var1 = new cust;
+
+	//OR
+
+	// Works fine, doesn’t require constructor
+	var1 = new cust();
+
+	// Notice error if you comment this line
+	cust* var = new cust(25);
+	return 0;
+}
